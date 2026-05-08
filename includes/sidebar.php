b1f12b2f500060f6a73706ab72c319dc9dbedbdf
Jean-Rene Branaa <jrbranaa@macbaby.localdomain>
2026-05-07 17:42:31 -0700

    sidebar with schedule widget

---
+++ includes/sidebar.php
@@ -0,0 +1,9 @@
+<aside class="sidebar">
+    <iframe
+        src="https://donatermater.com/widget"
+        width="100%"
+        height="50"
+        frameborder="0"
+        style="border:none;"
+    ></iframe>
+</aside>
