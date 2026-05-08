404e28f81c40c372f66338e466ae8a21693abee4
Jean-Rene Branaa <jrbranaa@macbaby.localdomain>
2026-05-07 17:57:29 -0700

    sidebar formatting

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,7 @@
     <iframe
         src="https://donatermater.com/widget"
         width="100%"
-        height="50"
+        height="100"
         frameborder="0"
         style="border:none;"
     ></iframe>
