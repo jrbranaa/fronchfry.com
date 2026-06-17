cc55dc936d28e058fe144dcc070615b080d9aac3
jrbranaa <jr@ossenx.com>
2026-06-16 22:56:54 -0700

    dynamic sidebar widget size

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,9 +1,16 @@
 <aside class="sidebar">
-    <iframe
+    <iframe id="yajigo-widget" 
         src="https://yajigo.com/widget"
-        width="100%"
-        height="75"
-        frameborder="0"
-        style="border:none;"
-    ></iframe>
+        scrolling="no" 
+        frameborder="0" 
+        style="width:100%;border:none;display:block;">
+    </iframe>
 </aside>
+
+<script>
+window.addEventListener('message', function(e) {
+  if (e.data && e.data.type === 'yajigo-resize') {
+    document.getElementById('yajigo-widget').style.height = e.data.height + 'px';
+  }
+});
+</script>
